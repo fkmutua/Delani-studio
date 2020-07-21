a10593a61dca4808632247fc0f43005b014d3df8
felix kasyoki mutua <fkmutua@safaricom.co.ke>
2020-07-21 04:16:34 -0700

    server and request handlers files included

--- requestHandler.js
+++ requestHandler.js
@@ -3,30 +3,30 @@ let Logger = require('./logger');
 let logger = new Logger('Request Handler');
 
 // function for handling the business logic for index.html
-function index (response){
+function index(response) {
     logger.info("Request handler for index was called.")
     console.log("Request handler for index was called.")
-    
+
     // Reading the index.html file and attaching the content to the response
-    fs.readFile("./public/index.html",function(err, data){
-        if (err){
-            logger.error(err)
-            console.log(err);  
-        } 
-        response.writeHead(200, {"Content-type": "text/html"});
-		response.write(data);
-		response.end();
+    fs.readFile("./public/index.html", function(err, data) {
+        if (err) {
+            logger.err(err)
+            console.log(err);
+        }
+        response.writeHead(200, { "Content-type": "text/html" });
+        response.write(data);
+        response.end();
     })
-    }
+}
 
 // function for handling the business logic for  portfolio.html
-function portfolio(response){
+function portfolio(response) {
     logger.info("Request for handler for portfolio was called.")
     console.log("Request for handler for portfolio was called.")
-    response.writeHead(200, {"Content-type": "text/plain"});
+    response.writeHead(200, { "Content-type": "text/plain" });
     response.write("These are some of our portfolio projects");
     response.end();
-    
+
 }
 
 
--- server.js
+++ server.js
@@ -4,25 +4,25 @@ let Logger = require('./logger');
 let logger = new Logger('Server');
 
 // A function to wrap our server functionality so that we can export it
-let start = function(route, handle){
+let start = function(route, handle) {
 
-	function onRequest(request, response){
-		// Extracting the pathname from the url requested
-		let pathname = url.parse(request.url).pathname
-		
-		console.log("Request for " + pathname + " has been received.")
-		logger.info("Request for " + pathname + " has been received with the request method " + request.method)
-		
-		// Inject the response object into the route function
-		route(handle, pathname, response);
+    function onRequest(request, response) {
+        // Extracting the pathname from the url requested
+        let pathname = url.parse(request.url).pathname
 
-	}
-	
-	let PORT = process.env.PORT || 8000;
+        console.log("Request for " + pathname + " has been received.")
+        logger.info("Request for " + pathname + " has been received with the request method " + request.method)
 
-	http.createServer(onRequest).listen(PORT);
-	logger.info(`Server has started on Port: ${PORT}`)
-	console.log(`Server has started on Port: ${PORT}`);
+        // Inject the response object into the route function
+        route(handle, pathname, response);
+
+    }
+
+    let PORT = process.env.PORT || 8000;
+
+    http.createServer(onRequest).listen(PORT);
+    logger.info(`Server has started on Port: ${PORT}`)
+    console.log(`Server has started on Port: ${PORT}`);
 }
 
 exports.start = start;
